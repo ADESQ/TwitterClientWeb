4aeab32a6270b6707a7a536767d6d3bda149084a
Pierre-Malherbe <pierre.malherbe@sylpheo.com>
2017-09-27 11:22:15 +0200

    [TECH] Initialize Spring Boot

---
+++ src/main/java/com/adesq/TwitterclientApplication.java
@@ -0,0 +1,12 @@
+package com.adesq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterclientApplication.class, args);
+	}
+}
