36c60016bdc0824a7ae615f52902a40fc96e2e73
Théo Beuze <theo.beuze@sylpheo.com>
2017-09-29 13:35:07 +0200

    Second real test

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 kill $(ps aux | grep -w twitterclient | awk '{print $2}')
 cd /var/www
-mvn clean install
-nohup java -jar ../target/*.jar > twitterClient.log 2>&1&
+nohup java -jar target/*.jar > twitterClient.log 2>&1&
 exit 0
